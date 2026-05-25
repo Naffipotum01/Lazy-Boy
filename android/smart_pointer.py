9387cfc4565edacb329515cad8856a37fe828e91
Naffipotum01 <Naffipotum01@users.noreply.github.com>
2026-05-24 21:56:24 -0500

    Fix Kivy 2.3.1 compat: remove unused TouchRippleBehavior import (removed in Kivy 2.3.0)

--- android/smart_pointer.py
+++ android/smart_pointer.py
@@ -2,7 +2,6 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.behaviors import TouchRippleBehavior
 from kivy.clock import Clock
 from kivy.metrics import dp
 from kivy.graphics import Color, RoundedRectangle
